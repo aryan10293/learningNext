578a0c5f80e802ff74fc1cc549c7111414ee6b9f
aondrej ryan <aondrejryan11@gmail.com>
2026-05-04 16:37:59 -0700

    testing

--- backend/config/neon.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import pkg from 'pg';
-const { Pool } = pkg;
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: false ,
-});
-export default pool;
--- backend/routes/main.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import main from "../controller/main";
-// import { requireAuth } from "../middleware/main";
-const router = express.Router();
-
-router.get("/test", main.doesThisWork);
-
-export default router;
-
